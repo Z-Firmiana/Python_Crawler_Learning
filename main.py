855c48cb89be28c28f4fc2bb6e29375fddebef23
Firmiana_Z <firmianakasugano@outlook.com>
2023-05-26 20:22:28 +0800

    Initial commit

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ import pandas as pd
 from DataLoad import Excel_Load_to_Array
 from Data_Input import test
 
-test(r"C:\Users\15194\Desktop\新建 Microsoft Excel 工作表.xlsx", r"https://service.ncepu.edu.cn/v2/matter/start?id=157")
+test(r"C:\Users\15194\Desktop\新建 Microsoft Excel 工作表.xlsx111111111111", r"https://service.ncepu.edu.cn/v2/matter/start?id=157")
+# 车市语句
