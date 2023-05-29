f0431bc2b1028647cb9bdb4a7faea17fca157071
Firmiana_Z_Gitee <firmianakasugano@outlook.com>
2023-05-29 16:04:58 +0800

    Merge remote-tracking branch 'gitee/master'
    # Conflicts:
    #	main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import pandas as pd
 from DataLoad import Excel_Load_to_Array
 from Data_Input import test
 
-test(r"C:\Users\15194\Desktop\新建 Microsoft Excel 工作表.xlsx", r"https://service.ncepu.edu.cn/v2/matter/start?id=157")
+# test(r"C:\Users\ZZXXT\Desktop\新建 Microsoft Excel 工作表.xlsx", r"https://service.ncepu.edu.cn/v2/matter/start?id=157")
 # 车市语句
 # ceshi
+
